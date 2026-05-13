6dcf4e1b5ac9e64839599f60f3c84e735c2aab4b
Nishant Jadhav <nishantjadhav.cs@gmail.com>
2026-05-13 16:36:57 +0530

    update: Re-arranged skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,24 +2,24 @@ import Reveal from './Reveal'
 import TiltedCard from './TiltedCard'
 
 const skills = [
-  ['JavaScript', 'javascript.png'],
-  ['Python', 'python.png'],
-  ['C++', 'c++.png'],
-  ['PHP', 'php.png'],
-  ['Django', 'django-1.png'],
-  ['Shopify', 'shopify.png'],
-  ['NodeJS', 'node.png'],
   ['React', 'react.png'],
+  ['JavaScript', 'javascript.png'],
+  ['Django', 'django-1.png'],
+  ['Python', 'python.png'],
+  ['Shopify', 'shopify.png'],
   ['Docker', 'docker.png'],
-  ['MySQL', 'mysql.png'],
-  ['MongoDB', 'mongodb.png'],
-  ['Git', 'git.png'],
   ['AWS', 'aws.png'],
-  ['HTML', 'html.png'],
-  ['CSS', 'css.png'],
+  ['Git', 'git.png'],
+  ['Node.js', 'node.png'],
+  ['PHP', 'php.png'],
+  ['C++', 'c++.png'],
+  ['MongoDB', 'mongodb.png'],
+  ['MySQL', 'mysql.png'],
+  ['HTML5', 'html.png'],
+  ['CSS3', 'css.png'],
   ['Linux', 'linux.png'],
   ['Google Analytics', 'analytics.png'],
-  ['MS Clarity', 'clarity.png'],
+  ['Microsoft Clarity', 'clarity.png'],
 ]
 
 function Skills() {
